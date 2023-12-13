75916f4608f66cd18f6294c6e336fc5133aaad11
pursue <>
2023-12-13 10:12:11 +0100

    vicuna

---
+++ vicuna/scripts/train_vicuna_13b.sh
@@ -0,0 +1,26 @@
+torchrun --nproc_per_node=8 --master_port=20001 fastchat/train/train_mem.py \
+    --model_name_or_path lmsys/vicuna-13b-v1.5-16k  \
+    --data_path ./elife \
+    --bf16 True \
+    --output_dir output_vicuna_13b \
+    --num_train_epochs 50 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 16 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "steps" \
+    --eval_steps 1500 \
+    --save_strategy "steps" \
+    --save_steps 1500 \
+    --save_total_limit 5 \
+    --learning_rate 5e-5 \
+    --weight_decay 0.1 \
+    --warmup_ratio 0.2 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --fsdp "full_shard auto_wrap offload" \
+    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
+    --tf32 True \
+    --model_max_length 16384 \
+    --gradient_checkpointing True \
+    --lazy_preprocess True
+
